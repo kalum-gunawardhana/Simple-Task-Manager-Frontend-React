01985f0ac0cf371c362ec1e2e7fe6a540565c255
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-29 22:46:29 +0530

    commit

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import api from "../services/api";
+import TaskItem from "./TaskItem";
+
+export default function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    api.get("/").then(res => setTasks(res.data));
+  }, []);
+
+  return (
+    <ul>
+      {tasks.map(task => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </ul>
+  );
+}
